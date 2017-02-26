15b0a3f62e79ccdc2df8f6e1aad5746934bb7be4
Diogo Salazar <daq@qad.com>
2017-02-26 13:30:59 -0800

    Added the option to initialize the plugin via data attributes; Fixed a bug when trying to update the settings on an existing instance; TODO: Add some sort of unit testing

--- src/jquery.fontcide.js
+++ src/jquery.fontcide.js
@@ -1,4 +1,5 @@
-;(function ($, window, document, undefined) {
+;
+(function ($, window, document, undefined) {
 
     "use strict";
 
@@ -7,90 +8,90 @@
         defaults = {
             enabled: true,
             compressor: 1,
-            minFontSize: Number.NEGATIVE_INFINITY,
-            maxFontSize: Number.POSITIVE_INFINITY,
+            min: Number.NEGATIVE_INFINITY,
+            max: Number.POSITIVE_INFINITY,
         },
         eventName = "resize." + pluginName + " orientationchange." + pluginName;
-    
+
     var plugins = [];
-    
-    $(window).on(eventName, function() {
-        $(plugins).each(function() {
+
+    $(window).on(eventName, function () {
+        $(plugins).each(function () {
             $(this).data("plugin_" + pluginName).resize();
         });
     });
-    
+
     var Plugin = function (element, options) {
         //Instance variables
         var settings = $.extend({}, defaults, options);
-        
+
         var $element = $(element);
-        
+
         var originalFontSize = $element.css("font-size");
         var fontSizeInline = ($element.attr("style") || "").indexOf("font-size") >= 0;
-        
+
         //Instance private methods
         function attach() {
-            plugins.push( element );
+            plugins.push(element);
         }
-        
+
         function detach() {
-            plugins = $(plugins).filter(function(){
+            plugins = $(plugins).filter(function () {
                 return this !== element;
             });
         }
-        
+
         //Instance public methods
         this.resize = function () {
             $(element).css("font-size",
-                Math.max(Math.min($(element).width() / (settings.compressor * 10), parseFloat(settings.maxFontSize)),
-                    parseFloat(settings.minFontSize)));
+                Math.max(Math.min($(element).width() / (settings.compressor * 10), parseFloat(settings.max)),
+                    parseFloat(settings.min)));
         };
-        this.set = function( newSetting, value ) {
+        this.set = function (newSetting, value) {
             var handlerStatus = settings.enabled;
-            
-            if( typeof( newSetting ) === "object" ) {
-                $.extend({}, settings, newSetting);
-            } else if( typeof( newSetting ) === "string" && value && settings[newSetting]) {
+
+            if (typeof (newSetting) === "object") {
+                $.extend(settings, newSetting);
+            } else if (typeof (newSetting) === "string" && value && settings[newSetting]) {
                 settings[newSetting] = value;
             } else {
                 return;
             }
-            
-            if( settings.enabled !== handlerStatus ) {
-                if( settings.enabled ) {
+
+            if (settings.enabled !== handlerStatus) {
+                if (settings.enabled) {
                     attach();
                 } else {
                     detach();
                 }
             }
-            
+
             this.resize();
         };
-        this.remove = function() {
+        this.remove = function () {
             detach();
             $(element).removeData("plugin_" + pluginName);
-            if( fontSizeInline ) {
+            if (fontSizeInline) {
                 $(element).css("font-size", originalFontSize);
             } else {
                 $(element).css("font-size", "");
             }
         };
-        
+
         //Initialization
         this.resize();
         attach();
     };
-    
+
     $.fn[pluginName] = function (options) {
-        if( this.length === 1 && this.eq(0).data("plugin_" + pluginName)) {
-            if( options ) {
-                this.eq(0).data("plugin_" + pluginName).set( options );
+        if (this.length === 1 && this.eq(0).data("plugin_" + pluginName)) {
+            if (options) {
+                this.eq(0).data("plugin_" + pluginName).set(options);
             } else {
                 return this.eq(0).data("plugin_" + pluginName);
             }
         }
-            
+
         return this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
                 $.data(this, "plugin_" +
@@ -98,5 +99,39 @@
             }
         });
     };
+
+    $(document).ready(function () {
+        var defaultKeys = Object.keys(defaults);
+        $("[data-" + pluginName + "]").each(function () {
+            
+            var $this = $( this );
+            var buildOptions = function() {
+                var options = {};
+                
+                defaultKeys.forEach(function (key) {
+                    var value = $this.attr("data-" + pluginName + "-" + key);
+                    if( value != null )
+                        options[key] = value;
+                    });
+                
+                return options;
+            };
+            
+            $this.fontcide(buildOptions());
+            
+            $this.attrchange({
+                callback: function(e) {
+                    if( e.attributeName.startsWith( "data-" + pluginName ) ) {
+                        if( e.attributeName === "data-" + pluginName ) {
+                            $this.fontcide().remove();
+                            $this.attrchange('remove');
+                        } else {
+                            $this.fontcide().set(buildOptions());
+                        }
+                    }
+                }
+            });
+        });
+    });
 
 })(jQuery, window, document);
